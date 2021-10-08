14a64432c0e997c2a3967d9673afaa859167f656
Masud Ahmed <masudahmed88@gmail.com>
2021-10-08 14:49:25 -0400

    added log in function

--- client/src/components/pages/Header/Header.js
+++ client/src/components/pages/Header/Header.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import Navigation from '../Navigation/Navigation';
 
 const Header = () => {
     return (
         <header className="jumbotron jumbotron-fluid">
             <h1 className="text-center">Battle Buddies! </h1>
             <h2 className="text-center"> Finding friends for the fight </h2>
-            <Navigation /> 
+            <div>
+                {
+                    loggedIn ? (
+                        <div>
+                            <button type="button" onClick={() => setLoggedIn(!loggedIn)}>
+                                Log out
+                            </button>
+                        </div>
+                    ) : (
+                        <div>
+                            <button type="button" onClick={() => setLoggedIn(!loggedIn)}>
+                                Log in
+                            </button>
+                        </div>
+                    )}
+            </div>
+
         </header>
     );
 }
