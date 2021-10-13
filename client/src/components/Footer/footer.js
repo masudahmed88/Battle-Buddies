4f0182b0eb698abf5dd7b4ae1c1f2df40b11aa9e
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-13 01:34:13 -0400

    added footer

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            
+  return (
+    <footer className="bg-dark text-light mt-0 pt-3" id="contact">
+      <div className="container">
+        <div className="row">
+          <div className="col-12"></div>
+          <p className="text-center mt-3">&copy;2021. Battle Buddies. All rights reserved</p>
         </div>
-    );
-}
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
