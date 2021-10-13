c02fc8847e158255f2ff69d0ebbde6e9871386aa
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-13 11:51:00 -0400

    editted forms

--- client/src/components/Header/header.js
+++ client/src/components/Header/header.js
@@ -1,33 +1,44 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Header ({loggedIn,setLoggedIn}){
-
-
-    return (
-        <header className="jumbotron jumbotron-fluid">
-            <h1 className="text-center">Battle Buddies! </h1>
-            <h2 className="text-center"> Finding friends for the fight </h2>
-            <div>
-                {
-                    loggedIn ? (
-                        <div>
-                            <button type="button" onClick={() => setLoggedIn(!loggedIn)}>
-                                Log out
-                            </button>
-                        </div>
-                    ) : (
-                        <div>
-                            <button type="button" onClick={() => setLoggedIn(!loggedIn)}>
-                                Log in
-                            </button>
-                        </div>
-                    )}
-            </div>
-
-        </header>
-    );
+export default function Header({ loggedIn, setLoggedIn }) {
+  return (
+    <header className="jumbotron jumbotron-fluid">
+      <h1 className="text-center">Battle Buddies! </h1>
+      <h2 className="text-center"> Finding friends for the fight </h2>
+      <div className="d-flex flex-row justify-content-end px-5">
+        {loggedIn ? (
+          <div>
+            <Link to="/">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={() => setLoggedIn(!loggedIn)}
+              >
+                {" "}
+                Log out
+              </button>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <Link to="/login">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={() => setLoggedIn(!loggedIn)}
+              >
+                Log in
+              </button>
+            </Link>
+            <Link to="/signup">
+              <button type="button" className="btn btn-secondary">
+                Sign Up
+              </button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </header>
+  );
 }
-
-
-
-
