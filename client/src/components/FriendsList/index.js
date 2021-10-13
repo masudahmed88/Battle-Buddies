612d0cc51711269cad244258328cd64b90ab086e
Masud Ahmed <masudahmed88@gmail.com>
2021-10-13 13:15:49 -0400

    updated profile page with dynamic assets

--- client/src/components/FriendsList/index.js
+++ client/src/components/FriendsList/index.js
@@ -1,46 +1,45 @@
 import React, { useEffect } from "react";
 import { useQuery } from "@apollo/client";
 
-import Game from "../Game/index";
+import friend from "../friend/index";
 import { useStoreContext } from "../../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../../utils/actions";
 import { QUERY_PRODUCTS } from "../../utils/queries";
 // import spinner from '../../assets/spinner.gif';
 
-function GameList() {
+function FriendsList() {
   const [state, dispatch] = useStoreContext();
 
-  const { loading, data } = useQuery(QUERY_COMMONGAME);
+  const { loading, data } = useQuery(QUERY_FRIEND);
 
   useEffect(() => {
     if (data) {
       dispatch({
-        type: UPDATE_GAME,
-        game: data.game,
+        type: UPDATE_FRIEND,
+        friend: data.friend,
       });
     }
   }, [data, dispatch]);
 
   return (
-    <div className="my-2">
-      <h2>Games:</h2>
-      {state.games.length ? (
+    <div>
+      {state.friend.length ? (
         <div className="flex-row">
-          {games.map((game) => (
-            <Game
-              key={game._id}
-              _id={game._id}
-              image={game.image}
-              name={game.name}
+          {friends.map((friend) => (
+            <Friend
+              key={friend._id}
+              _id={friend._id}
+              image={friend.image}
+              name={friend.name}
             />
           ))}
         </div>
       ) : (
-        <h3>You don't have any games yet!</h3>
+        <h3>You don't have any friends yet!</h3>
       )}
       {/* {loading ? <img src={spinner} alt="loading" /> : null} */}
     </div>
   );
 }
 
-export default GameList;
+export default friendList;
--- client/src/components/pages/Profile/profile.js
+++ client/src/components/pages/Profile/profile.js
@@ -1,4 +1,6 @@
 import React from "react";
+import FriendList from "../../FriendsList";
+import GameList from "../../GameList";
 
 const Profile = () => {
   return (
@@ -9,164 +11,14 @@ const Profile = () => {
             <h2 className="text-center">My Friends</h2>
           </div>
           <div class="list-group">
-            <a href="#" class="list-group-item list-group-item-action">
-              Mike
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              Masud
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              Nick
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              Adwait
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              Kevin
-            </a>
+            <FriendList />
           </div>
         </div>
         <div className="col-8">
           <h2 className="text-center">My Games</h2>
           {/* First Row of Games */}
           <div className="row my-3">
-            <div className="col-3">
-              <div className="card">
-                <img
-                  className="card-img-top"
-                  src={"https://media.giphy.com/media/y0NFayaBeiWEU/giphy.gif"}
-                  alt="Card image cap"
-                />
-                <div className="card-body">
-                  <h3 className="text-center">Title</h3>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make up the bulk of the
-                    card's content.
-                  </p>
-                </div>
-              </div>
-            </div>
-            {/* Game 2 */}
-            <div className="col-3">
-              <div className="card">
-                <img
-                  className="card-img-top"
-                  src={"https://media.giphy.com/media/y0NFayaBeiWEU/giphy.gif"}
-                  alt="Card image cap"
-                />
-                <div className="card-body">
-                  <h3 className="text-center">Title</h3>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make up the bulk of the
-                    card's content.
-                  </p>
-                </div>
-              </div>
-            </div>
-            {/* Game 3 */}
-            <div className="col-3">
-              <div className="card">
-                <img
-                  className="card-img-top"
-                  src={"https://media.giphy.com/media/y0NFayaBeiWEU/giphy.gif"}
-                  alt="Card image cap"
-                />
-                <div className="card-body">
-                  <h3 className="text-center">Title</h3>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make up the bulk of the
-                    card's content.
-                  </p>
-                </div>
-              </div>
-            </div>
-            {/* Game 4 */}
-            <div className="col-3">
-              <div className="card">
-                <img
-                  className="card-img-top"
-                  src={"https://media.giphy.com/media/y0NFayaBeiWEU/giphy.gif"}
-                  alt="Card image cap"
-                />
-                <div className="card-body">
-                  <h3 className="text-center">Title</h3>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make up the bulk of the
-                    card's content.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* Second Row of Games */}
-          <div className="row my-3">
-            <div className="col-3">
-              <div className="card">
-                <img
-                  className="card-img-top"
-                  src={"https://media.giphy.com/media/y0NFayaBeiWEU/giphy.gif"}
-                  alt="Card image cap"
-                />
-                <div className="card-body">
-                  <h3 className="text-center">Title</h3>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make up the bulk of the
-                    card's content.
-                  </p>
-                </div>
-              </div>
-            </div>
-            {/* Game 2 */}
-            <div className="col-3">
-              <div className="card">
-                <img
-                  className="card-img-top"
-                  src={"https://media.giphy.com/media/y0NFayaBeiWEU/giphy.gif"}
-                  alt="Card image cap"
-                />
-                <div className="card-body">
-                  <h3 className="text-center">Title</h3>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make up the bulk of the
-                    card's content.
-                  </p>
-                </div>
-              </div>
-            </div>
-            {/* Game 3 */}
-            <div className="col-3">
-              <div className="card">
-                <img
-                  className="card-img-top"
-                  src={"https://media.giphy.com/media/y0NFayaBeiWEU/giphy.gif"}
-                  alt="Card image cap"
-                />
-                <div className="card-body">
-                  <h3 className="text-center">Title</h3>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make up the bulk of the
-                    card's content.
-                  </p>
-                </div>
-              </div>
-            </div>
-            {/* Game 4 */}
-            <div className="col-3">
-              <div className="card">
-                <img
-                  className="card-img-top"
-                  src={"https://media.giphy.com/media/y0NFayaBeiWEU/giphy.gif"}
-                  alt="Card image cap"
-                />
-                <div className="card-body">
-                  <h3 className="text-center">Title</h3>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make up the bulk of the
-                    card's content.
-                  </p>
-                </div>
-              </div>
-            </div>
+          <GameList />
           </div>
         </div>
       </div>
