c38ea39728e3c693d1cd164500d79fde6393d71c
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-14 00:16:12 -0400

    exported gameprofile and added declared fields to their places

--- client/src/components/GameProfile/index.js
+++ client/src/components/GameProfile/index.js
@@ -1,24 +1,23 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import Game from "../Game";
 
-function GameProfile(item){
-    const{ name, image, category, rating, dateReleased}
+function GameProfile(item) {
+  const { name, image, category, rating, dateReleased } = item;
 
-    return(
-        <div class="card ml-5  mt-5 p-3" style="width: 18rem;">
-            <div class="card-header rounded-top">
-              <h2 class="text-center">{Featured}</h2>
-            </div>
-            <img
-              class="card-img-top"
-              src={"https://placekitten.com/200/200"}
-              alt="Card image cap"
-            />
-            <div class="card-body">
-              <p class="card-text text-center"> Game Category: {category}</p>
-              <p class="card-text text-center">Game Rating: {rating}</p>
-              <p class="card-text text-center">Release Date: {relase}</p>
-            </div>
-          </div>
-    )
+  return (
+    <div class="card ml-5  mt-5 p-3" style="width: 18rem;">
+      <div class="card-header rounded-top">
+        <h2 class="text-center">{name}</h2>
+      </div>
+      <img class="card-img-top" src={image} alt="Card image cap" />
+      <div class="card-body">
+        <p class="card-text text-center"> Game Category: {category}</p>
+        <p class="card-text text-center">Game Rating: {rating}</p>
+        <p class="card-text text-center">Release Date: {dateReleased}</p>
+      </div>
+    </div>
+  );
 }
+
+export default GameProfile;
