356bed234c7d9076ffbefa629d222a19e2546eed
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-14 01:14:21 -0400

    fixed incorrect method of adding class

--- client/src/components/GameProfile/index.js
+++ client/src/components/GameProfile/index.js
@@ -5,15 +5,15 @@ function GameProfile(item) {
   const { name, image, category, rating, dateReleased } = item;
 
   return (
-    <div class="card ml-5  mt-5 p-3">
-      <div class="card-header rounded-top">
-        <h2 class="text-center">{name}</h2>
+    <div className="card ml-5  mt-5 p-3">
+      <div className="card-header rounded-top">
+        <h2 className="text-center">{name}</h2>
       </div>
-      <img class="card-img-top" src={image} alt="Card image cap" />
-      <div class="card-body">
-        <p class="card-text text-center"> Game Category: {category}</p>
-        <p class="card-text text-center">Game Rating: {rating}</p>
-        <p class="card-text text-center">Release Date: {dateReleased}</p>
+      <img className="card-img-top" src={image} alt="Card image cap" />
+      <div className="card-body">
+        <p className="card-text text-center"> Game Category: {category}</p>
+        <p className="card-text text-center">Game Rating: {rating}</p>
+        <p className="card-text text-center">Release Date: {dateReleased}</p>
       </div>
     </div>
   );
