6958a79e352bbfc5975d2a3e366d7f9055525ead
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-14 00:39:47 -0400

    added battle buddy finder button to game profile page

--- client/src/components/pages/Game/game.js
+++ client/src/components/pages/Game/game.js
@@ -1,30 +1,31 @@
 import React from "react";
 import GameProfile from "../../GameProfile";
-import GameList from "../../GameList";
 import FriendsInCommon from "../../FriendsInCommon";
+import BattleBuddyFinder from "../../BattleBuddyFinder";
 
 const Profile = () => {
   return (
     <div className="container-fluid">
       <div className="row ">
         {/* Game Info */}
-        <div className="col-4">
+        <div className="col-6">
           <div>
             <h2 className="text-center">Game Info</h2>
           </div>
           <GameProfile />
         </div>
         {/* Friends Who Have This Game */}
-        <div className="col-4">
+        <div className="col-3">
           <div>
             <h2 className="text-center">Friends Who Have This Game </h2>
           </div>
           <ul className="list-group">
             <FriendsInCommon />
           </ul>
-          <GameList />
         </div>
-        <div className="col-4"></div>
+        <div className="col-3">
+          <BattleBuddyFinder />
+        </div>
       </div>
     </div>
   );
