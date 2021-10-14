0b2e4cc10e0bf809cb5c516863b523800a3b0c27
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-14 01:12:38 -0400

    added a div around button

--- client/src/components/BattleBuddyFinder/index.js
+++ client/src/components/BattleBuddyFinder/index.js
@@ -4,7 +4,11 @@ import { Link } from "react-router-dom";
 function BattleBuddyFinder(item) {
   const { name } = item;
 
-  return <button className="btn btn-primary">Find your Battle Buddy for {name}</button>;
+  return (
+    <div>
+      <button className="btn btn-primary">Find your Battle Buddy for {name}</button>
+    </div>
+  );
 }
 
 export default BattleBuddyFinder;
