e11fcdee1f41e7068da997c1b78de2e5dab140dc
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-18 00:50:40 -0400

    saving

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -4,6 +4,72 @@ const { User, SteamAccount, Game } = require("../models");
 // const gameSeedData = require("./steamGames.json");
 
 db.once("open", async () => {
+  const games = await Game.insertMany([
+    {
+      appid: "1",
+      gameName: "Back 4 Blood",
+      image: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/back4blood_rlzx2c.jpg",
+    },
+    {
+      appid: "2",
+      gameName: "The Jackbox Party Pack 8",
+      image:
+        "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/jackboxpartypack8_elgar8.jpg",
+    },
+    {
+      appid: "3",
+      gameName: "Disco Elysium",
+      image: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/disco_gvxvml.jpg",
+    },
+    {
+      appid: "4",
+      gameName: "Halo The Master Chief Collection",
+      image:
+        "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/halomastercheif_uqiy3n.jpg",
+    },
+    {
+      appid: "5",
+      gameName: "Ori and the Will of the Wisps ",
+      image: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/disco_gvxvml.jpg",
+    },
+    {
+      appid: "6",
+      gameName: "Bubble People",
+      image:
+        "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/bubblepeople_szkgv8.jpg",
+    },
+    {
+      appid: "7",
+      gameName: "Baldur's Gate",
+      image: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/baldursgate_nnrylo.jpg",
+    },
+    {
+      appid: "8",
+      gameName: "Rust",
+      image: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/rust_fr6flb.jpg",
+    },
+    {
+      appid: "9",
+      gameName: "New World",
+      image: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525506/newworld_y1kvqv.jpg",
+    },
+    {
+      appid: "10",
+      gameName: "Rebel Inc Escalation",
+      image: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525506/rebelinc_udbgwz.jpg",
+    },
+    {
+      appid: "11",
+      gameName: "The Rift Breaker",
+      image: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525506/riftbreaker_wueurs.jpg",
+    },
+    {
+      appid: "12",
+      gameName: "Phasmophobia",
+      image: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525506/phasmo_hrbsyg.jpg",
+    },
+  ]);
+
   await User.deleteMany();
 
   await User.create({
@@ -13,6 +79,7 @@ db.once("open", async () => {
     password: "password12345",
     steamID: "76561197960435530",
     isPremium: true,
+    games: [games[1].appid, games[3].appid, games[8].appid, games[12].appid, games[5].appid],
   });
 
   await User.create({
@@ -22,77 +89,10 @@ db.once("open", async () => {
     password: "password12345",
     steamID: "76561197960435530",
     isPremium: true,
+    games: [games[1].appid, games[3].appid, games[8].appid, games[12].appid, games[5].appid],
   });
 
   console.log("users seeded");
-
-  const games = await Game.insertMany([
-    {
-      appId: `01`,
-      name: "Back 4 Blood",
-      images: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/back4blood_rlzx2c.jpg",
-    },
-    {
-      appId: `02`,
-      name: "The Jackbox Party Pack 8",
-      images:
-        "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/jackboxpartypack8_elgar8.jpg",
-    },
-    {
-      appId: `03`,
-      name: "Disco Elysium",
-      images: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/disco_gvxvml.jpg",
-    },
-    {
-      appId: `04`,
-      name: "Halo The Master Chief Collection",
-      images:
-        "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/halomastercheif_uqiy3n.jpg",
-    },
-    {
-      appId: `05`,
-      name: "Ori and the Will of the Wisps ",
-      images: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/disco_gvxvml.jpg",
-    },
-    {
-      appId: `06`,
-      name: "Bubble People",
-      images:
-        "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/bubblepeople_szkgv8.jpg",
-    },
-    {
-      appId: `07`,
-      name: `Baldur's Gate`,
-      images:
-        "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/baldursgate_nnrylo.jpg",
-    },
-    {
-      appId: `08`,
-      name: "Rust",
-      images: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/rust_fr6flb.jpg",
-    },
-    {
-      appId: `09`,
-      name: "New World",
-      images: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525506/newworld_y1kvqv.jpg",
-    },
-    {
-      appId: `10`,
-      name: "Rebel Inc Escalation",
-      images: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525506/rebelinc_udbgwz.jpg",
-    },
-    {
-      appId: `11`,
-      name: "The Rift Breaker",
-      images:
-        "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525506/riftbreaker_wueurs.jpg",
-    },
-    {
-      appId: `12`,
-      name: `Phasmophobia`,
-      images: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525506/phasmo_hrbsyg.jpg",
-    },
-  ]);
 
   console.log("games seeded");
 
