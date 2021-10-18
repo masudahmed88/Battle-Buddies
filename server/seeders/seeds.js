35d34fa9b346e653dad73df4447e54e33daf85c1
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-17 23:37:57 -0400

    added appId to games

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -28,55 +28,67 @@ db.once("open", async () => {
 
   const games = await Game.insertMany([
     {
+      appId: `01`,
       name: "Back 4 Blood",
       images: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/back4blood_rlzx2c.jpg",
     },
     {
+      appId: `02`,
       name: "The Jackbox Party Pack 8",
       images:
         "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/jackboxpartypack8_elgar8.jpg",
     },
     {
+      appId: `03`,
       name: "Disco Elysium",
       images: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/disco_gvxvml.jpg",
     },
     {
+      appId: `04`,
       name: "Halo The Master Chief Collection",
       images:
         "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/halomastercheif_uqiy3n.jpg",
     },
     {
+      appId: `05`,
       name: "Ori and the Will of the Wisps ",
       images: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/disco_gvxvml.jpg",
     },
     {
+      appId: `06`,
       name: "Bubble People",
       images:
         "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/bubblepeople_szkgv8.jpg",
     },
     {
+      appId: `07`,
       name: `Baldur's Gate`,
       images:
         "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/baldursgate_nnrylo.jpg",
     },
     {
+      appId: `08`,
       name: "Rust",
       images: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525507/rust_fr6flb.jpg",
     },
     {
+      appId: `09`,
       name: "New World",
       images: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525506/newworld_y1kvqv.jpg",
     },
     {
+      appId: `10`,
       name: "Rebel Inc Escalation",
       images: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525506/rebelinc_udbgwz.jpg",
     },
     {
+      appId: `11`,
       name: "The Rift Breaker",
       images:
         "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525506/riftbreaker_wueurs.jpg",
     },
     {
+      appId: `12`,
       name: `Phasmophobia`,
       images: "https://res.cloudinary.com/dz9z8kntj/image/upload/v1634525506/phasmo_hrbsyg.jpg",
     },
